4d2f8f7ae21e67340febd7db57b18d4062c2c4e0
DarshanGowdaN <84765649+DarshanGowdaN@users.noreply.github.com>
2021-06-30 09:59:19 +0530

    Update Program-9.c

--- Program-9.c
+++ Program-9.c
@@ -1,3 +1,4 @@
+
 //linear search 
 //INPUT- 4 1 2 3 4 3
 //OUTPUT - 3
@@ -28,7 +29,7 @@ int main()
     }
   }
   if (c == n)
-    printf("%d is not present in the array", );
+    printf("%d is not present in the array", search );
 
   return 0;
 }
